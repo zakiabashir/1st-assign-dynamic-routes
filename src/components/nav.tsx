de3615ae723cdd3778d09fde2147db78be42ca5d
zakiabashir <nshafeys0@gmail.com>
2024-11-09 06:49:25 +0500

    push my code

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -6,10 +6,10 @@ import React from 'react';
 export default function Navbar() {
     return (
         
-        <div className='text-2xl p-4 bold text-red-900 bg-gradient-to-br from-red-400 via-gray-400 to-teal-400'>
+        <div className='text-2xl  p-4 bold text-red-900 bg-gradient-to-br from-red-400 via-gray-400 to-teal-400'>
           <Card className='grid p-8 m-4 bg-gradient-to-br from-red-900 via-gray-900 to-teal-900'> 
               <h1 className='flex p-6  justify-center width-[200px] text-red-900  bottom-0 bg-gradient-to-br from-red-400 via-gray-400 to-teal-400'>Dynamic country listing</h1></Card>
-            <ul className='flex gap-2 justify-center width-[200px] text-red-900   bottom-0 bg-gradient-to-br from-red-400 via-gray-400 to-teal-400 '>
+            <ul className=' flex flex-wrap gap-2 justify-center width-[200px] text-red-900   bottom-0 bg-gradient-to-br from-red-400 via-gray-400 to-teal-400 '>
                <Card className='grid p-8 m-8 bg-gradient-to-br from-red-900 via-gray-900 to-teal-900'> <li className='text-red-900 p-2  bg-gradient-to-br from-red-400 via-gray-400 to-teal-400'><Link href="/country/canada">Canada</Link></li></Card>
                <Card className='grid p-8 m-8 bg-gradient-to-br from-red-900 via-gray-900 to-teal-900'> <li className='text-red-900 p-2 bg-gradient-to-br from-red-400 via-gray-400 to-teal-400'><Link href="/country/germany">Germany</Link></li></Card>
                <Card className='grid p-8 m-8 bg-gradient-to-br from-red-900 via-gray-900 to-teal-900'> <li className='text-red-900 p-2  bg-gradient-to-br from-red-400 via-gray-400 to-teal-400'><Link href="/country/japan">Japan</Link></li></Card>
